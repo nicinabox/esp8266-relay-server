2ccdabad6d4cab59ab311538fbb5f50bafd61b26
Nic Haynes <nic@nicinabox.com>
2018-09-03 23:20:15 -0500

    Refactor to use for gate control instead of relay control

--- esp8266-relay-server.ino
+++ esp8266-relay-server.ino
@@ -2,47 +2,65 @@
 #include <ESP8266WebServer.h>
 
 // Esp8266 pinouts
-#define ESP8266_GPIO2   2  // Blue LED.
-#define ESP8266_GPIO4   4  // Relay control.
-#define ESP8266_GPIO5   5  // Optocoupler input.
+#define ESP8266_GPIO2   2  // LED
+#define ESP8266_GPIO4   4  // Relay
+#define ESP8266_GPIO5   5  // Optocoupler input
 #define LED_PIN         ESP8266_GPIO2
 
 const int PORT = 80;
 const char* ssid = "WIFI_SSID";
 const char* password = "WIFI_PASSWORD";
 
-const String STATES[] = { "open", "closed" };
-volatile int state = 0;
+const String STATUSES[] = { "open", "closed" };
+volatile int initialRelayState = 0;
 
 ESP8266WebServer server(PORT);
 
-void setState(int nextState) {
-  state = nextState;
-  digitalWrite(ESP8266_GPIO4, state);
+void setRelayState(int nextState) {
+  digitalWrite(ESP8266_GPIO4, nextState);
+}
+
+void cycleRelay() {
+  setRelayState(1);
+  delay(100);
+  setRelayState(0);
+}
+
+String getStatus() {
+  return STATUSES[digitalRead(ESP8266_GPIO5)];
+}
+
+bool isClosed() {
+  return getStatus() == "closed";
+}
+
+bool isOpen() {
+  return getStatus() == "open";
 }
 
 void handleRoot() {
-  String payload = "{ \"state\": " + STATES[state] + " }";
+  String payload = "{ \"status\": " + getStatus() + " }";
   server.send(200, "text/json", payload);
 }
 
 void handleOpen() {
-  setState(0);
+  if (isClosed()) {
+    cycleRelay();
+  }
+
   server.send(204);
 }
 
 void handleClose() {
-  setState(1);
+  if (isOpen()) {
+    cycleRelay();
+  }
+
   server.send(204);
 }
 
 void handleCycle() {
-  int originalState = state
-  int nextState = 1 - state
-
-  setState(nextState);
-  delay(100);
-  setState(originalState);
+  cycleRelay();
 
   server.send(204);
 }
@@ -75,7 +93,8 @@ void setupRelay() {
   pinMode(ESP8266_GPIO4, OUTPUT);
   pinMode(ESP8266_GPIO5, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(ESP8266_GPIO4, state);
+
+  digitalWrite(ESP8266_GPIO4, initialRelayState);
 }
 
 void setup() {
